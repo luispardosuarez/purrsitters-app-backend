b700607161c97a270dd93f847b4ddf8eabed959a
Luis Pardo <luispardosuarez@gmail.com>
2024-02-22 15:20:50 +0100

    add hamcrest dependency

---
+++ src/main/java/com/purrsitters/purrsittersappbackend/PurrsittersAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.purrsitters.purrsittersappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurrsittersAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurrsittersAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/purrsitters/purrsittersappbackend/PurrsittersAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purrsitters.purrsittersappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurrsittersAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
